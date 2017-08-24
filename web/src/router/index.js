80cdec91d50377fa18fe1198586ab0e8713b9117
Daniel <djs66256@163.com>
2017-08-24 17:20:09 +0800

    base router

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
+let baseRouter = ''
+if (process.env === 'production') {
+  baseRouter = '/validator'
+}
+
 Vue.use(Router)
 
 export default new Router({
+  base: baseRouter,
   routes: [
     {
       path: '/',
